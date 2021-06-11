e8955a6dfe711116ee9ce4e3d0ba19e6e7b0f02b
miguelfdz <miguelfdzm08@gmail.com>
2021-06-11 17:41:50 -0500

    Added test to add todos

--- spec/features/user_visits_homepage_spec.rb
+++ spec/features/user_visits_homepage_spec.rb
@@ -4,6 +4,6 @@ feature "User visits homepage" do
   scenario "succesfully" do
     visit root_path
 
-    expect(page).to have_css 'h1', text: 'Todos'
+    expect(page).to have_css 'h1', text: 'My To-do list'
   end
 end
